a2f0e7e9a72415af9be2a2ef1a5e08dfd2be40d0
0xTD <thecryptosparrow@proton.me>
2024-03-10 22:43:57 +0000

    fix precision problem

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-verify');
+require('@nomicfoundation/hardhat-chai-matchers');
+require("hardhat-contract-sizer");
+require('hardhat-dependency-compiler');
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('hardhat-tracer');
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
