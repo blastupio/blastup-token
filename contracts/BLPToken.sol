65857ccd1eb4bdd75bbd61ef2428ce65bb8bf33f
0xTD <thecryptosparrow@proton.me>
2024-03-13 19:10:21 +0000

    fix - optimization and zero address check

--- contracts/BLPToken.sol
+++ contracts/BLPToken.sol
@@ -6,9 +6,13 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract BLPToken is ERC20 {
     // Initial supply of token 1B, precision is 18
     uint256 constant INITIAL_SUPPLY = 1_000_000_000 * (10**18);
-    address public daoAddress;
+    address immutable public daoAddress;
 
     constructor(address _daoAddress) ERC20("BlastUP Token", "BLP") {
+        if (_daoAddress == address(0)) {
+            return;
+        }
+
         daoAddress = _daoAddress;
         _mint(daoAddress, INITIAL_SUPPLY);
     }
